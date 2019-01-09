aafba9366b24dbc5fdaf90c8252b99b29fee85e4
Thiago Novaes Sampaio dos Santos <e-tnovaes@tce.sp.gov.br>
2019-01-09 16:12:05 -0200

    Retornando null

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 require_once("config.php");
 
-$sql = new Sql;
+$root = new Usuario();
+$root->loadById(5);
 
-$users = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($users);
+echo $root;
