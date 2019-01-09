0c684057649c1eeda120afcb7cd86e3b030b57b6
Thiago Novaes Sampaio dos Santos <e-tnovaes@tce.sp.gov.br>
2019-01-09 16:39:38 -0200

    Funções de Lista

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
 require_once("config.php");
-
+/*
 $root = new Usuario();
 $root->loadById(5);
+*/
 
-echo $root;
+/*
+$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+/*
+$busca = Usuario::serach("Th");
+echo json_encode($busca);
+*/
+
+$usuario = new Usuario();
+$usuario->login("Thiago", "123123");
+
+echo $usuario;
