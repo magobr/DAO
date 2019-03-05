daaa7cd6ce6584309fec50ff1b8cc6c37b62b8d0
Thiago <novaesthago@gmail.com>
2019-03-05 19:54:45 -0300

    Novos Metodos implementados

--- index.php
+++ index.php
@@ -13,5 +13,32 @@ $lista = Usuario::getList();
 echo json_encode($lista);
 */
 
+/*
 $search = Usuario::search("th");
 echo json_encode($search);
+*/
+
+/*
+$usuarios = new Usuario();
+$usuarios->login("root", "!@#$");
+
+echo $usuarios;
+*/
+
+/*
+$aluno = new Usuario('Aluno2', 'Senha2');
+$aluno->Insert();
+echo $aluno;
+*/
+
+/*
+$newUser = new Usuario();
+$newUser->loadById(7);
+$newUser->update('Professor', '!@#$');
+
+echo $newUser;
+*/
+
+$user = new Usuario();
+$user->loadById(7);
+$user->delete();
